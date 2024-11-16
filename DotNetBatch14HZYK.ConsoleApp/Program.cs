d2d8cbded6bd6c630e4ab0895550e95644b38387
HeinZayarKyaw108 <hlay0329@gmail.com>
2024-11-16 10:43:55 +0630

    Add project files.

---
+++ DotNetBatch14HZYK.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter Username: ");
+//string username = Console.ReadLine();
+
+
+//Blog
+
+//ID
+//Title
+//Author
+//Content
