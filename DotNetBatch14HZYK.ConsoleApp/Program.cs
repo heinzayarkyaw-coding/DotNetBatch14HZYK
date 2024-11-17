fcca2aaf0dad1e9b57dfbc634d036d886121e2c3
HeinZayarKyaw108 <hlay0329@gmail.com>
2024-11-17 08:57:47 +0630

    feat: ADO.NET Read

--- DotNetBatch14HZYK.ConsoleApp/Program.cs
+++ DotNetBatch14HZYK.ConsoleApp/Program.cs
@@ -1,13 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
-Console.WriteLine("Enter Username: ");
-//string username = Console.ReadLine();
+SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+connectionStringBuilder.DataSource = "LAPTOP-MMRAUNPQ"; //severname
+connectionStringBuilder.InitialCatalog = "Textdb"; //database name
+connectionStringBuilder.UserID = "sa";
+connectionStringBuilder.Password = "Hlay1082001";
+connectionStringBuilder.TrustServerCertificate = true;
+
+SqlConnection connection = new SqlConnection(connectionStringBuilder.ConnectionString);
+Console.WriteLine("Connecting Opening...");
+connection.Open();
+Console.WriteLine("Connecting Open.");
+
+Console.WriteLine("Connecting Closing...");
+connection.Close();
+Console.WriteLine("Connecting Close.");
+
+//Ctrl+.
+
+
+Console.ReadLine();
+
+
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+connection.Close();
+
+
+//DataTable
+//DataRow
+//DataColumn
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogID"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+}
+
+
+Console.ReadLine() ;
+
 
 
 //Blog
 
 //ID
 //Title
+
 //Author
 //Content
+
+
+
+
+//MSSQL
+
